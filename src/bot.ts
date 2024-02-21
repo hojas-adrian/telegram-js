0f3b0d84c705cacc5e36676b1510ffcf5ddec31b
hojas.adrian <hojas.adrian@gmail.com>
2024-02-21 09:19:57 -0500

    Fix: info middleware set as global

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,8 @@
 import { apiThrottler, Bot } from "../deps.ts";
 import commands from "./composer/commands.ts";
-import info from "./composer/info.ts";
 import errorHandler from "./handlers/error-handler.ts";
-import { BOT_TOKEN } from "./helpers/constants.ts";
+import { BOT_TOKEN, COMMANDS, DATA } from "./helpers/constants.ts";
+import { filterCommands, setDescription } from "./helpers/utils.ts";
 import evalHandler from "./handlers/on-eval-handler.ts";
 import boundaryHandler from "./handlers/boundary-handler.ts";
 import { matchFilter } from "../deps.ts";
@@ -17,6 +17,18 @@ notChannel.on("msg:entities:pre").errorBoundary(boundaryHandler, evalHandler);
 
 notChannel.use(commands);
 
-bot.use(info);
+await bot.api.setMyDescription(
+  `${setDescription(`${`JSbot`}`)}\n\n${DATA.joinUs}`,
+);
+
+await bot.api.setMyShortDescription(DATA.shortDescription);
+
+await bot.api.setMyCommands(filterCommands(COMMANDS, "all_private_chats"), {
+  scope: { type: "all_private_chats" },
+});
+
+await bot.api.setMyCommands(filterCommands(COMMANDS, "all_group_chats"), {
+  scope: { type: "all_group_chats" },
+});
 
 bot.catch(errorHandler);
